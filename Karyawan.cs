9f2ba10d43ea59446597e7f414d8e0b7af7d7fd0
febrico3353 <81755075+febrico3353@users.noreply.github.com>
2021-04-13 12:05:02 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrogaman3353
+{
+    class Karyawan
+    {
+        int NIK { get; set; }
+        String Nama { get; set; }
+        int GajiBulanan { get; set; }
+
+        public Karyawan(int NIK, string Nama, int GajiBulanan)
+        {
+            if(GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+            }
+
+            this.NIK = NIK;
+            this.Nama = Nama;
+        }
+
+        public static void NaikGaji(Karyawan obj1, Karyawan obj2)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Alhamdulillahnaik gaji 10%");
+            Console.WriteLine("");
+            obj1.GajiBulanan += obj1.GajiBulanan * 10 / 100;
+            obj2.GajiBulanan += obj2.GajiBulanan * 10 / 100;
+
+        }
+        public static void Show(Karyawan obj1, Karyawan obj2)
+        {
+            Console.WriteLine("No NIK/Nama\t\tGajiBulanan");
+            Console.WriteLine("1 {0}{1}\t{2}", obj1.NIK, obj1.Nama, obj1.GajiBulanan);
+            Console.WriteLine("2 {0}{1}\t{2}", obj2.NIK, obj2.Nama, obj2.GajiBulanan);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrogaman3353
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kar1 = new Karyawan(180601306, "Budi", 3300000);
+            Karyawan kar2 = new Karyawan(180601307, "Surya", 4400000);
+
+            Karyawan.Show(kar1, kar2);
+            Karyawan.NaikGaji(kar1, kar2);
+            Karyawan.Show(kar1, kar2);
+        }
+    }
+}
